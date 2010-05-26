2c0a7c1ae30f70e7f18cb452d8aa0829e450d640
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-05-26 22:38:35 +0900

    remove Util::CheckerTimer since the monitor uses one shot periodic timer.

--- lib/isono/util.rb
+++ lib/isono/util.rb
@@ -10,36 +10,6 @@ module Isono
     end
     module_function :gen_id
 
-    class CheckerTimer < EventMachine::PeriodicTimer
-      def initialize(time, &blk)
-        @interval = time
-        @code = proc {
-          begin
-            blk.call
-          rescue => e
-            Wakame.log.error(e)
-          end
-        }
-        stop
-      end
-
-      def start
-        if !running?
-          @cancelled = false
-          schedule
-        end
-      end
-
-      def stop
-        @cancelled = true
-      end
-
-      def running?
-        !@cancelled
-      end
-
-    end
-     
   end
 end
 
