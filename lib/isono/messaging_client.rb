9d9646e155f276b8f72c89ab31aebf5d44bfd1f2
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-10-05 20:09:53 +0900

    update MessagingClient to use new node modules.

--- lib/isono/messaging_client.rb
+++ lib/isono/messaging_client.rb
@@ -1,36 +1,111 @@
 # -*- coding: utf-8 -*-
 
 require 'isono'
+require 'eventmachine'
 
 module Isono
-  class MessagingClient < Agent
+  # @example Sync RPC call with object method. 
+  # mc = MessagingClient.start
+  # puts mc.sync_request('endpoint', 'func1', xxxx, xxxx)
+  # puts mc.sync_request('endpoint', 'func2', xxx, xxx)
+  #
+  # @example Sync RPC call using delegated object
+  # mc = MessagingClient.start
+  # endpoint = mc.sync_rpc('endpoint')
+  # endpoint.func1(xxxx, xxxx)
+  # endpoint.func2(xxx, xxx)
+  #
+  class MessagingClient < Node
+    include Logger
 
-    def initialize
-      m = Manifest.new(Dir.pwd) {
-        node_name 'command'
-        node_id Util.gen_id
+    def self.start(amqp_uri, manifest=nil)
+      node = self.new(manifest)
 
-        manager ManagerModules::EventChannel
-        manager ManagerModules::RpcChannel
+      if EventMachine.reactor_thread?
+        EventMachine.schedule {
+          node.connect(amqp_uri)
+        }
+      else
+        q = ::Queue.new
+        EventMachine.schedule {
+          node.connect(amqp_uri) { |type|
+            q << type
+          }
+        }
+        case q.deq
+        when :success
+        when :error
+          raise "Connection failed: #{amqp_uri}"
+        end
+      end
+
+      node
+    end
+
+    def stop
+      if connected?
+        close {
+          EventMachine.schedule {
+            EventMachine.stop
+          }
+        }
+      end
+    end
+
+    
+    def initialize(m=nil, &blk)
+      m ||= Manifest.new(Dir.pwd) {
+        node_name 'msgclient'
+        node_instance_id Util.gen_id
+
+        load_module NodeModules::EventChannel
+        load_module NodeModules::RpcChannel
       }
+      m.instance_eval(&blk) if blk
       super(m)
     end
 
-    def send_event(event, data, sender=agent_id)
-      EventMachine.schedule {
-        ManagerModules::EventChannel.instance.publish(event, sender, data)
-      }
-    end
+    class RpcSyncDelegator
+      attr_reader :endpoint
+      
+      def initialize(rpc, endpoint, opts={})
+        @rpc = rpc
+        @endpoint = endpoint
+        @opts = {:timeout=>0.0, :oneshot=>false}.merge(opts)
+      end
 
-    def sync_command(namespace, command, args={})
-      ManagerModules::RpcChannel.instance.sync_request(namespace, command, args)
-    end
+      private
+      def method_missing(m, *args)
+        if @opts[:oneshot]
+          oneshot_request(m, *args)
+        else
+          normal_request(m, *args)
+        end
+      end
 
-    def async_command(namespace, command, args={}, &blk)
-      EventMachine.schedule {
-        ManagerModules::RpcChannel.instance.async_request(namespace, command, args, &blk)
-      }
+      def oneshot_request(m, *args)
+        @rpc.request(@endpoint, m, *args) { |req|
+          req.oneshot = true
+        }
+      end
+
+      def normal_request(m, *args)
+        q = ::Queue.new
+        req = @rpc.sync_request(@endpoint, m, *args)
+
+        req.wait
+      end
     end
     
+    def sync_rpc(endpoint, opts={})
+      rpc = NodeModules::RpcChannel.new(self)
+      RpcSyncDelegator.new(rpc, endpoint, opts)
+    end
+
+    def sync_request(endpoint, key, args)
+      rpc = NodeModules::RpcChannel.new(self)
+      rpc.sync_request(endpoint, key, args)
+    end
+
   end
 end
