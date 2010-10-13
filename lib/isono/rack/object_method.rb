5861007b5148459da981fe3fbd409ec663386e86
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-10-13 16:06:18 +0900

    reduced functionalty of Rack::ObjectMethod to simply dispatch args and method's result.

--- lib/isono/rack/object_method.rb
+++ lib/isono/rack/object_method.rb
@@ -5,27 +5,14 @@ module Rack
   class ObjectMethod
     include Logger
     
-    def initialize(obj, opts={})
+    def initialize
       @obj = obj
-      @opts = {:extract_args=>false}.merge(opts)
     end
     
     def call(req, res)
-      @request = req
-      @response = res
-      begin
-        m = @obj.method(req.key)
-        raise UnknownMethodError, "#{req.key}" if m.nil?
-        if @opts[:extract_args]
-          ret = m.arity > 0 ? m.call(*res.args) : m.call
-          res.response(ret)
-        else
-          m.call
-          raise ResponseIncompleteError unless res.responded?
-        end
-      ensure
-        @request = @response = nil
-      end
+      m = @obj.method(req.command)
+      raise UnknownMethodError, "#{req.command}" if m.nil?
+      res.response(m.arity > 0 ? m.call(*req.args) : m.call)
     end
     
   end
