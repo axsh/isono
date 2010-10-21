3caabddb030277a6707430a5a7dd3ae3b220b12a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2010-10-21 16:19:25 +0900

    fix the bug that the arguments were not passed when the method had variable length arguments.

--- lib/isono/rack/object_method.rb
+++ lib/isono/rack/object_method.rb
@@ -12,7 +12,7 @@ module Rack
     def call(req, res)
       m = @obj.method(req.command)
       raise UnknownMethodError, "#{req.command}" if m.nil?
-      res.response(m.arity > 0 ? m.call(*req.args) : m.call)
+      res.response(m.arity.abs > 0 ? m.call(*req.args) : m.call)
     end
     
   end
