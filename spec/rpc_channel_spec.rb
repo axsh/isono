35c17bfd0cc37c009dd903322c77cd1aa2b39a37
Masahiro Fujiwara <m-fujiwara@axsh.net>
2011-03-08 22:33:19 +0900

    update spec

--- spec/rpc_channel_spec.rb
+++ spec/rpc_channel_spec.rb
@@ -6,71 +6,60 @@ require 'isono'
 MM = Isono::NodeModules
 include Isono
 
-
 def client_connect(main_cb, pre_cb=nil, post_cb=nil)
-  em_fork(proc {
-            manifest = Manifest.new(File.expand_path('../', __FILE__)) {
-              node_name :cli
-              node_instance_id   :xxx
-              
-              load_module MM::EventChannel
-              load_module MM::RpcChannel
-            }
-            c = Node.new(manifest)
-            pre_cb.call if pre_cb
-            c.connect('amqp://localhost/') {
-              main_cb.call(c)
-            }
-          },post_cb)
+  manifest = Manifest.new(File.expand_path('../', __FILE__)) {
+    node_name :cli
+    node_instance_id   :xxx
+    
+    load_module MM::EventChannel
+    load_module MM::RpcChannel
+  }
+  c = Node.new(manifest)
+  pre_cb.call if pre_cb
+  c.connect('amqp://localhost/') {
+    main_cb.call(c)
+  }
 end
 
 
 def svr_connect(main_cb, pre_cb=nil, post_cb=nil)
-  em_fork(proc {
-            manifest = Manifest.new(File.expand_path('../', __FILE__)) {
-              node_name :endpoint
-              node_instance_id   :xxx
-              
-              load_module MM::EventChannel
-              load_module MM::RpcChannel
-            }
-            c = Node.new(manifest)
-            pre_cb.call if pre_cb
-            c.connect('amqp://localhost/') {
-              main_cb.call(c)
-            }
-          },post_cb)
+  manifest = Manifest.new(File.expand_path('../', __FILE__)) {
+    node_name :endpoint
+    node_instance_id   :xxx
+    
+    load_module MM::EventChannel
+    load_module MM::RpcChannel
+  }
+  c = Node.new(manifest)
+  pre_cb.call if pre_cb
+  c.connect('amqp://localhost/') {
+    main_cb.call(c)
+  }
 end
 
-describe "RpcChannel: client and server" do
-  
-  it "creates connection" do
-    done = false
 
+describe Isono::NodeModules::RpcChannel do
+  em "creates connection" do
+    done = []
     svr_connect(proc{ |c|
                   c.close {
-                    done = true
-                    EM.stop
+                    done << 1
                   }
-                }, nil, proc{
-                  done.should.equal true
                 })
-
+    
     client_connect(proc {|c|
                      c.close {
-                       done = true
-                       EM.stop
+                       done << 1
                      }
-                   }, nil, proc {
-                     done.should.equal true
                    })
-
-    Process.waitall.all? { |s|
-      s[1].exitstatus == 0
-    }.should.equal true
+    
+    EM.tick_loop {
+      #done.should.equal [1, 1]
+      EM.stop if done == [1, 1]
+    }
   end
   
-  it "send async request" do
+  em "send async request" do
     svr_connect(proc{|c|
                   rpc = MM::RpcChannel.new(c)
                   rpc.register_endpoint('endpoint1', Isono::Rack::Map.build { |t|
@@ -83,28 +72,23 @@ describe "RpcChannel: client and server" do
                                             response.response({:code=>1})
                                           }
                                         })
-                }, proc{
                 })
-    sleep 1
     client_connect(proc {|c|
                      rpc = MM::RpcChannel.new(c)
                      req0 = rpc.request('endpoint1', 'kill', 'arg1', "arg2", "arg3") { |req|
                        req.on_success { |res|
                          req0.ticket.should.equal req.ticket
                          res[:code].should.equal 1
-                         c.close { EM.stop }
+                         c.close
                        }
                      }
-                   },proc {
-                   })
+                   }
+                   )
     
-    Process.waitall.all? { |s|
-      s[1].exitstatus == 0
-    }.should.equal true
   end
 
 
-  it "request timeout" do
+  em "request timeout" do
     client_connect(proc {|c|
                      rpc = MM::RpcChannel.new(c)
                      rpc.request('test', 'test1') { |req|
@@ -115,31 +99,27 @@ describe "RpcChannel: client and server" do
                        }
                      }
                    })
-    
-    Process.waitall.all? { |s|
-      s[1].exitstatus == 0
-    }.should.equal true
   end
 
 
-  it "decorated dispatcher" do
+  em "use Isono::Rack dispatcher" do
+    func1_count = 0
     svr_connect(proc{|c|
                   rpc = MM::RpcChannel.new(c)
                   endpoint1 = Isono::Rack::Map.build { |t|
                     t.map('kill') {
-                      EM.add_timer(0.5) { EM.stop }
+                      func1_count.should.equal 10
+                      EM.next_tick { EM.stop }
                     }
                     t.map('func1') {
                       request.args[0].should.equal 'arg1'
-                      sleep 2
+                      func1_count += 1
                       response.response({:code=>1})
                     }
                   }
 
                   rpc.register_endpoint('endpoint1', Isono::Rack::ThreadPass.new(endpoint1))
-                }, proc{
                 })
-    sleep 1
     client_connect(proc {|c|
                      rpc = MM::RpcChannel.new(c)
                      10.times { |no|
@@ -149,58 +129,42 @@ describe "RpcChannel: client and server" do
                            req0.complete_status.should.equal :success
                            #((req0.completed_at - req0.sent_at) > 1.5).should.be.true
                            res[:code].should.equal 1
-                           puts "#{no} elapesed: #{req0.elapsed_time}"
+                           #puts "#{no} elapesed: #{req0.elapsed_time}"
                            if no == 9
                              rpc.request('endpoint1', 'kill') do |req|
-                               #req.oneshot = true
-                               req.on_success {
-                                 EM.next_tick { EM.stop }
+                               req.on_success { |res|
+                                 res[:code].should.equal 1
                                }
                              end
                            end
                          }
                        }
                      }
-                   },proc {
                    })
     
-    Process.waitall.all? { |s|
-      s[1].exitstatus == 0
-    }.should.equal true
-
-    sleep 2
   end
 
-  it "catch remote exception" do
+  em "catch remote exception" do
     svr_connect(proc{|c|
                   rpc = MM::RpcChannel.new(c)
                   rpc.register_endpoint('endpoint1', Isono::Rack::Map.build { |t|
                                           t.map('kill') {
-                                            p Thread.current == EM.reactor_thread
-                                            EM.next_tick { EM.stop }
                                             raise StandardError, "message"
                                             response.response({:code=>1})
                                           }
                                         })
                 }, proc{
                 })
-    sleep 1
+    client_connect(proc {|c|
+                     rpc = MM::RpcChannel.new(c)
+                     rpc.request('endpoint1', 'kill') { |req|
+                       req.on_error { |e|
+                         e.should.equal({:error_type=>'StandardError', :message=>'message'})
+                         EM.stop
+                       }
+                     }
+                   })    
 
-    Process.fork {
-      Thread.new { EM.run }
-      m = MessagingClient.start('amqp://localhost/')
-      begin
-        m.request('endpoint1', 'kill', 'arg1', "arg2", "arg3")
-        false.should.equal true
-      rescue MM::RpcChannel::RpcError => e
-        puts "come rescue error"
-        true.should.equal true
-      end
-    }
-    
-    Process.waitall.all? { |s|
-      s[1].exitstatus == 0
-    }.should.equal true
   end
   
 end
